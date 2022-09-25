41f207b0476893034a17684dc893d6aa8154f9bc
Ivan <i.khyzhniak@kyrrex.com>
2022-09-25 12:41:26 +0300

    fix

--- rabbit.rb
+++ rabbit.rb
@@ -3,15 +3,15 @@ require "bunny"
 module Rabbit
   extend self
 
-	def publish(data, routing_key: 'default')
-		begin
-			conn = Bunny.new(hostname:  ENV.fetch('RABBITMQ_HOST', 'localhost'))
-			conn.start
-			$ch = conn.create_channel
-		rescue StandardError
-			sleep 1
-			retry
-		end unless $ch
-		$ch.default_exchange.publish(data, routing_key: routing_key)
-	end
+  def publish(data, routing_key: 'default')
+    begin
+      conn = Bunny.new(hostname:  ENV.fetch('RABBITMQ_HOST', 'localhost'))
+      conn.start
+      $ch = conn.create_channel
+    rescue StandardError
+      sleep 1
+      retry
+    end unless $ch
+    $ch.default_exchange.publish(data, routing_key: routing_key)
+  end
 end
